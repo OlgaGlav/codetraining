1dfa22a8f2cc1f4d35ce198ab2a3009fd2a35475
Olga <pin007@tut.by>
2022-09-15 16:32:01 +0300

    fix game

--- src/main/java/by/glavdel/pasha/Game.java
+++ src/main/java/by/glavdel/pasha/Game.java
@@ -17,51 +17,29 @@ public class Game {
 
         for (int i = 0; i < field.length; ++i) {
             for (int j = 0; j < field[i].length; ++j) {
-                int sum = 0;
-                if (i != 0 && j != 0 && i != field.length - 1 && j != field[i].length - 1) {
-                    sum = newStateForCenter(field, i, j);
-                } else {
-                    sum = newStateForBorder(field, i, j);
-                }
-
+                int sum = countNeighbors(field, i, j);
                 newGeneration[i][j] = analizeState(sum, field[i][j]);
             }
         }
-
         return newGeneration;
+    }
+
+    private static int countNeighbors(int[][] field, int i, int j) {
+        int sum = 0;
+        for (int k = i - 1; k <= i + 1; k++) {
+            if (k >= 0 && k < field.length) {
+                for (int l = j - 1; l <= j + 1; l++) {
+                    if (l >= 0 && l < field[0].length) {
+                        sum += field[k][l];
+                    }
+                }
+            }
+        }
+        return sum - field[i][j];
     }
 
     private static int analizeState(int sum, int element) {
         return sum != 3 && (sum != 2 || element != 1) ? 0 : 1;
-    }
-
-    private static int newStateForCenter(int[][] field, int i, int j) {
-        return field[i - 1][j - 1] + field[i - 1][j] + field[i - 1][j + 1] + field[i][j - 1] + field[i][j + 1] + field[i + 1][j - 1] + field[i + 1][j] + field[i + 1][j + 1];
-    }
-
-    private static int newStateForBorder(int[][] field, int i, int j) {
-        int koefI = 1;
-        int koefJ = 1;
-        int sum = 0;
-        if (i != 0) {
-            koefI = -1;
-        }
-
-        if (j != 0) {
-            koefJ = -1;
-        }
-
-        if (i == 0 && j == 0 || i == field.length - 1 && j == 0 || i == 0 && j == field[0].length - 1 || i == field.length - 1 && j == field[0].length - 1) {
-            sum = field[i + koefI][j] + field[i][j + koefJ] + field[i + koefI][j + koefJ];
-        } else if (i != 0 && i != field.length - 1) {
-            if (j == 0 || j == field[0].length - 1) {
-                sum = field[i - koefI][j] + field[i + koefI][j] + field[i - koefI][j + koefJ] + field[i][j + koefJ] + field[i + koefI][j + koefJ];
-            }
-        } else {
-            sum = field[i][j + koefJ] + field[i][j - koefJ] + field[i + koefI][j - koefJ] + field[i + koefI][j] + field[i + koefI][j + koefJ];
-        }
-
-        return sum;
     }
 }
 
